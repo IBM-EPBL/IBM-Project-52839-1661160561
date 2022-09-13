1322289275efa8f795d8b1ebc004e7cba6ed9953
surya <suryatj1234@gmail.com>
2022-09-13 23:18:08 +0530

    assignment1

---
+++ Assignments/suryatj/firstass/list.py
@@ -0,0 +1,25 @@
+#Create a list
+from re import S
+
+
+surya = [1,2,3]
+
+#insert int at a position
+surya[0] = 0
+
+#Delete the first occurance
+surya.pop(0)
+
+#Insert at the end
+surya.append(4)
+
+#Sort the list
+surya.sort()
+
+#pop the last element
+surya.pop()
+
+#Reverse the list
+surya.reverse()
+
+print(surya)
---
+++ Assignments/suryatj/firstass/string.py
@@ -0,0 +1,10 @@
+str = "surya"
+
+#concatinate strings
+print(str+" TJ")
+
+#reverse the string
+print(str[::-1])
+
+#slice the string
+print(str[0:2])
