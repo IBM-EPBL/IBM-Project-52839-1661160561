ab0ecb1bb901caa28267860bd655d8668b930ca2
surya <suryatj1234@gmail.com>
2022-11-09 11:00:52 +0530

    surya assignment update

--- Assignments/Suryatj - Team Member/Assignment 2/app.py
+++ Assignments/Suryatj - Team Member/Assignment 2/app.py
@@ -1,91 +1,99 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import re
 import ibm_db
-import ibm_db_dbi
-
-conn_string = input("Enter Connection String : ")
-
-try:
-    ibm_db_conn = ibm_db.connect(conn_string, " ", " ")
-    print("connected")
-except Exception as e:
-    print(e)
-
-conn = ibm_db_dbi.Connection(ibm_db_conn)
+import re
 
 app = Flask(__name__)
+app.secret_key = 'secret'
+conn = ibm_db.connect(
+    "DATABASE=bludb;"
+    "HOSTNAME=6667d8e9-9d4d-4ccb-ba32-21da3bb5aafc.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;"
+    "PORT=30376;SECURITY=SSL;"
+    "SSLServerCertificate=DigiCertGlobalRootCA.crt;"
+    "PROTOCOL=TCPIP;"
+    "UID=ywf94293;"
+    "PWD=soBm6H5RCfodjrea", "", "")
 
-@app.route('/')
-def index():
-	return redirect(url_for('register'))
-	
-@app.route('/register', methods =['GET', 'POST'])
+print("Connected to database: ", conn)
+print("Connection successful.")
+
+
+@app.route('/', methods=['POST', 'GET'])
 def register():
-	msg = ''
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'rollnumber' in request.form :
-		username = request.form['username']
-		password = request.form['password']
-		email = request.form['email']
-		rollnumber = request.form['rollnumber']
-		cursor = conn.cursor()
-		select = f"SELECT * FROM user WHERE (username = \'{username}\') OR (rollnumber = \'{rollnumber}\') OR (email = \'{email}\');"
-		print(select)
-		cursor.execute(select)
-		account = cursor.fetchone()
-		if account:
-			msg = 'Account already exists !'
-		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-			msg = 'Invalid email address !'
-		elif not re.match(r'[A-Za-z0-9]+', username):
-			msg = 'Username must contain only characters and numbers !'
-		elif not username or not password or not email or not rollnumber:
-			msg = 'Please fill out the form !'
-		else:
-			insert = f"INSERT INTO user VALUES (\'{rollnumber}\', \'{email}\', \'{username}\', \'{password}\');"
-			cursor.execute(insert)
-			conn.commit()
-			msg = 'You have successfully registered !'
-		# return render_template('login.html', msg = msg)
-		return redirect(url_for('login'))
-	elif request.method == 'POST':
-		msg = 'Please fill out the form !'
-		return render_template('register.html', msg = msg)
-	return render_template('register.html', msg = msg)
-	# return redirect(url_for('login'))
+    msg = ''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        sql = "SELECT * FROM USERS WHERE USERNAME = ?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt, 1, username)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print(account)
+        print(username, password, email)
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+            print(msg)
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+            print(msg)
 
-@app.route('/login', methods =['GET', 'POST'])
+        elif not username or not password or not email:
+            msg = 'Please fill out the form !'
+            print(msg)
+
+        else:
+            insert_sql = 'INSERT INTO users(username, email, password) VALUES (?, ?, ?)'
+            prep_stmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(prep_stmt, 1, username)
+            ibm_db.bind_param(prep_stmt, 2, email)
+            ibm_db.bind_param(prep_stmt, 3, password)
+            ibm_db.execute(prep_stmt)
+            print("User created successfully")
+            msg = 'You have successfully registered !'
+            return render_template('login.html', msg=msg)
+
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg=msg)
+
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-	msg = ''
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-		username = request.form['username']
-		password = request.form['password']
-		select = f"SELECT * FROM user WHERE username = \'{username}\' AND password = \'{password}\';"
-		print(select)
-		cursor = conn.cursor()
-		cursor.execute(select)
-		account = cursor.fetchone()
-		print(account)
-		print(type(account))
-		if account:
-			session['loggedin'] = True
-			session['id'] = account[0]
-			session['username'] = account[2]
-			msg = 'Logged in successfully !'
-			return render_template('index.html', msg = msg)
-		else:
-			msg = 'Incorrect username / password !'
-	return render_template('login.html', msg = msg)
+    msg = ''
+    if request.method == 'GET':
+        return render_template('login.html', msg=msg)
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        sql = "SELECT * FROM users WHERE username =? AND password = ?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt, 1, username)
+        ibm_db.bind_param(stmt, 2, password)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print(account)
+        print(username, password)
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['USERNAME']
+            session['username'] = account['USERNAME']
+            msg = 'Logged in successfully !'
+            return render_template('home.html', msg=msg, username=username)  # Redirect to home page
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg=msg)
+
 
 @app.route('/logout')
 def logout():
-	session.pop('loggedin', None)
-	session.pop('id', None)
-	session.pop('username', None)
-	return redirect(url_for('login'))
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
-	app.secret_key = '01234'
-	app.config['SESSION_TYPE'] = 'filesystem'
-	app.run()
-Foo
+    app.run(host='0.0.0.0')
